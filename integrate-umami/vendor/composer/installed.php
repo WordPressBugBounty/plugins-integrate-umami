54bc10d40f01a63e621cdb49e38a2f519f5cbb9a
Sean Murphy <sean@iamseanmurphy.com>
2024-12-23 19:16:51 +0000

    0.8.1

---
+++ integrate-umami/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ancozockt/integrate-umami',
+        'pretty_version' => '0.8.1',
+        'version' => '0.8.1.0',
+        'reference' => '9ed8a3dbda8b7eb86341905385bc095a1dd483a1',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'ancozockt/integrate-umami' => array(
+            'pretty_version' => '0.8.1',
+            'version' => '0.8.1.0',
+            'reference' => '9ed8a3dbda8b7eb86341905385bc095a1dd483a1',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
